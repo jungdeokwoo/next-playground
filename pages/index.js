75d5e4d8ebeb3201666060c0cac9fe4f30297a15
jungdeokwoo <dogsocks@dogsocksui-MacBookPro.local>
2022-10-17 18:50:17 +0900

    [FIX] readme 수정
    readme 내용 수정

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <h1 className="title">
         Read <Link href="/posts/first-post">this page!</Link>
       </h1>
