c0822dff6c02c97fd2730ecb678180a594baba04
jungdeokwoo <dogsocks@dogsocksui-MacBookPro.local>
2022-10-19 11:17:59 +0900

    [ADD]스타일컴포넌트
    스타일 컴포넌트 연습해보기

--- README.md
+++ README.md
@@ -33,3 +33,22 @@
 - pages폴더 하부에 posts 폴더를 추가한 후 그 내부에 first-post.js 파일을 추가하면 기본 url(localhost:3000/)에 추가적으로 /post/first-post 페이지가 추가된다.
 
 - post 폴더 내부에 index.js 파일을 생성하게되면 /post 경로의 페이지가 생성된다
+
+### CSS 적용하기
+
+<hr>
+
+`동일 폴더 안에 CSS 파일을 추가한 후 해당 컴포넌트에서 import 해서 사용`
+
+- css 파일 이름은 파일명.modules.css 로 작성해주어야 css 를 적용할 수 있다.
+- public 폴더 내 image 폴더에 image 파일을 저장하여 해당 경로의 이미지를 사용할 수 있다.
+- html의 image 태그가 아닌 next에서 제공하는 Image 태그를 활용하는것이 좋다 (lazy-loading)
+
+### styled-components 적용해보기
+
+<hr>
+
+`react에서는 style-components 라이브러리만 설치해주었으면 되었으나 Next에서는 babel plugin도 함께 설치해주어야 한다.`
+
+- babel 플러그인 설치 없이 style-components만 사용하는 경우에는 SSR을 하면서 html이 먼저 노출 된 후에 css 파일이 나중에 적용되기 때문에 화면이 깜빡거리는듯한 현상이 생긴다.
+- 최상단 폴더에 .babelrc 파일 생성 후 babel 설정을 해주고 pages 폴더내에 \_document.js 파일을 생성하여 css파일을 불러와 덮어주도록 설계해준다.
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
+import styled from "styled-components";
 
 export default function Home() {
   return (
@@ -8,13 +8,18 @@ export default function Home() {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>deokwoo</p>
+      <HomeSection>
+        <p>Hello World</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
-      </section>
+      </HomeSection>
     </Layout>
   );
 }
+
+const HomeSection = styled.section`
+  font-size: 1.2rem;
+  line-height: 1.5;
+`;
