db806bbd3d67563ba6cb10939584beb8f048fa22
jungdeokwoo <dogsocks@dogsocksui-MacBookPro.local>
2022-11-25 19:10:43 +0900

    [ADD]next Login
    소셜로그인이 아닌 로그인 처리방식

--- pages/about.js
+++ pages/about.js
@@ -4,19 +4,26 @@ import Header from 'components/Header'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
+import { getSession, useSession } from 'next-auth/react'
+import { unstable_getServerSession } from 'next-auth'
+import { authOptions } from './api/auth/[...nextauth]'
 
-const About = () => {
+const About = ({ data }) => {
   const router = useRouter()
   const getPreview = () => {
     fetch('http://localhost:3000/api/productList')
     // router.push('/contact')
   }
+  const { data: session, status } = useSession()
+  console.log(session, '/////', status, '///////', data)
 
   return (
     <AboutTitle>
       <Link href="/about">
         <a>About</a>
       </Link>
+      <br></br>
+      {session ? <p>login success</p> : <p>Login Plz</p>}
       <button onClick={getPreview}>go contact</button>
     </AboutTitle>
   )
@@ -34,6 +41,16 @@ const AboutTitle = styled.h1`
     color: black;
   }
 `
+
+export async function getServerSideProps(context) {
+  const session = await unstable_getServerSession(
+    context.req,
+    context.res,
+    authOptions,
+  )
+  // const session = await getSession()
+  return { props: { data: session } }
+}
 
 About.getLayout = function getLayout(page) {
   return (
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,15 +1,52 @@
 import NextAuth from 'next-auth/next'
 import GitHubProvider from 'next-auth/providers/github'
+import CredentialsProvider from 'next-auth/providers/credentials'
 
-export default NextAuth({
+const USER = [
+  {
+    id: '1',
+    name: 'asdf',
+    password: 'asdf',
+    email: 'jsmith@example.com',
+    image: 'https://avatars.githubusercontent.com/u/97271725?v=4',
+  },
+]
+
+export const authOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        const user = USER.filter(
+          el =>
+            el.name === credentials.username &&
+            el.password === credentials.password,
+        )[0]
+        if (user) {
+          return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            image: user.image,
+          }
+        } else {
+          return null
+        }
+      },
     }),
     // Provider.GitHub({
     //   clientId: process.env.GITHUB_ID,
     //   clientSecret: process.env.GITHUB_SECRET,
     // }),
   ],
-})
+}
+
+export default NextAuth(authOptions)
